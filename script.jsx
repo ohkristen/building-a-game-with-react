1d378673adecf15ab09585f7867b3175ee5690af
Kristen Gunn <ohkristen@yahoo.com>
2016-02-26 14:44:58 -0800

    changed the GO button to become a counter with a getInitialState and a this.state reference in the component.  This will render the number of click on the face of the button

--- script.jsx
+++ script.jsx
@@ -1,7 +1,12 @@
-React.createClass({
+var Button = React.createClass({
+  getInitialState: function() {
+    return({counter: 0});
+  },
   render: function(){
     return (
-      <button>Go</button>
+      <button>{this.state.counter}</button>
     )
   }
 })
+
+React.render(<Button />, document.getElementById("root"));
